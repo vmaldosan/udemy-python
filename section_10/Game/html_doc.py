21e36ca73c8bb6ab89349c5a6d376d059e0bee69
Victor Maldonado <vmaldosan@gmail.com>
2020-12-08 23:07:49 +0000

    Add HTML Doc class and write contents to file

--- section_10/Game/html_doc.py
+++ section_10/Game/html_doc.py
@@ -8,8 +8,9 @@ class Tag(object):
   def __str__(self):
     return '{0.startTag}{0.contents}{0.endTag}'.format(self)
 
-  def display(self):
-    print(self)
+  def display(self, file=None):
+    print(self, file=file)
+
 
 class DocType(Tag):
 
@@ -17,12 +18,13 @@ class DocType(Tag):
     super().__init__('!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" http://www.w3.org/TR/html4/strict.dtd', '')
     self.endTag = ''
 
+
 class Head(Tag):
 
   def __init__(self):
     super().__init__('head', '')
 
-  
+
 class Body(Tag):
 
   def __init__(self):
@@ -33,8 +35,35 @@ class Body(Tag):
     newTag = Tag(name, contents)
     self._bodyContents.append(newTag)
 
-  def display(self):
+  def display(self, file=None):
     for tag in self._bodyContents:
       self.contents += str(tag)
 
-    super.display()
+    super().display(file=file)
+
+
+class HtmlDoc(object):
+
+  def __init__(self):
+    self._docType = DocType()
+    self._head = Head()
+    self._body = Body()
+
+  def addTag(self, name, contents):
+    self._body.addTag(name, contents)
+  
+  def display(self, file=None):
+    self._docType.display(file=file)
+    print('<html>', file=file)
+    self._head.display(file=file)
+    self._body.display(file=file)
+    print('</html>', file=file)
+
+
+if __name__ == '__main__':
+  myPage = HtmlDoc()
+  myPage.addTag('h1', 'Main heading')
+  myPage.addTag('h2', 'Sub-heading')
+  myPage.addTag('p', 'This is a paragraph that will appear on the page')
+  with open('test.html', 'w') as testDoc:
+    myPage.display(file=testDoc)
