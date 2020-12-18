c4099aa52335c1a838f9ae1e2aa1448513395606
Victor Maldonado <vmaldosan@gmail.com>
2020-12-18 17:53:05 +0000

    Create, insert and retrieve contacts

---
+++ section_11/contacts.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+db = sqlite3.connect('contacts.sqlite')
+db.execute('CREATE TABLE IF NOT EXISTS contacts (name TEXT, phone INTEGER, email TEXT)')
+db.execute("INSERT INTO contacts VALUES ('Victor', 23452345, 'victor@email.com')")
+db.execute("INSERT INTO contacts VALUES ('Brian', 1234, 'brian@myemail.com')")
+
+cursor = db.cursor()
+cursor.execute('SELECT * FROM contacts')
+
+# print(cursor.fetchall())
+print(cursor.fetchone())
+print(cursor.fetchone())
+print(cursor.fetchone())
+
+for row in cursor:
+  print(row)
+
+cursor.close()
+db.close()
